22e37cd3ded8480fad0809f36a856d4e8ff954d0
Aviral Upadhyay <aviralupadhyay@ymail.com>
2021-04-07 13:22:08 -0700

    test

--- MotionTracker.py
+++
@@ -1,7 +0,0 @@
-import cv2
-import mediapipe as mp
-class MotionTracker:
-    mpPose, mpDraw = mp.solutions.pose, mp.solutions.drawing_utils
-    def __init__(self):
-        self.pose = self.mpPose.Pose()
-
